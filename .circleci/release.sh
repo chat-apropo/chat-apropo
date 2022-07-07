d9944c70272b7b5f9bc4c66e4f142b4e94cf26d3
matheusfillipe <matheusfillipeag@gmail.com>
2022-07-07 03:19:08 -0300

    Basic CI

---
+++ .circleci/release.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+VERSION=$(git describe --tags --abbrev=0)
+wget https://github.com/tcnksm/ghr/releases/download/v0.14.0/ghr_v0.14.0_linux_amd64.tar.gz
+tar -xvzf ghr_*.tar.gz
+mv ghr_*_amd64 ghr
+
+zip linuxx64.zip build/linux/x64/release/bundle/*
+
+
+mkdir -p ghrelease
+mv *.zip ghrelease/
+mv build/app/outputs/apk/release/app-armeabi-v7a-release.apk ghrelease/
+mv build/app/outputs/apk/release/app-arm64-v8a-release.apk ghrelease/
+mv build/app/outputs/apk/release/app-x86_64-release.apk ghrelease/
+
+echo "RELEASE VERSION $VERSION"
+./ghr/ghr -t ${GITHUB_TOKEN} -u ${CIRCLE_PROJECT_USERNAME} -r ${CIRCLE_PROJECT_REPONAME} -c ${CIRCLE_SHA1} -delete ${VERSION} ./ghrelease/
